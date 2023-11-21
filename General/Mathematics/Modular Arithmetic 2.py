3ea226344409080cdd61ee489b6201d80f6eb84d
Bharpreet Singh <114077360+BharpreetSingh@users.noreply.github.com>
2023-11-21 14:55:19 +0000

    MA-2

---
+++ General/Mathematics/Modular Arithmetic 2.py
@@ -0,0 +1,4 @@
+#This problem deals with Fermat's Little Theorem #
+#Since 273246787654 is not divisible by 65537 #
+#The theorem states that a^p-1 ≡ 1 (mod p) = a^p-1 % p = 1 #
+#Therefore our answer was just 1 #
